739e7fdd3e454454b29bafd793820de09a672c7a
swansavage@github.com <swansavage@gmail.com>
2022-09-29 12:54:03 -0500

    added some form validation for new polls

--- src/Components/CreatePollForm.svelte
+++ src/Components/CreatePollForm.svelte
@@ -1,9 +1,37 @@
 <script>
   import Button from "../Shared/Button.svelte";
   let fields = { question: "", answerA: "", answerB: "" };
+  let errors = { question: "", answerA: "", answerB: "" };
+  let valid = false;
 
   const submitHandler = () => {
-    console.log(fields);
+    valid = true;
+    // validate question
+    if (fields.question.trim().length < 5) {
+      valid = false;
+      errors.question = "Question must be at least 5 characters long";
+    } else {
+      errors.question = "";
+    }
+    // validate answer A
+    if (fields.answerA.trim().length < 1) {
+      valid = false;
+      errors.answerA = "Answer A cannot be empty";
+    } else {
+      errors.answerA = "";
+    }
+    // validate answer B
+    if (fields.answerB.trim().length < 1) {
+      valid = false;
+      errors.answerB = "Answer B cannot be empty";
+    } else {
+      errors.answerB = "";
+    }
+
+    // add new poll
+    if (valid) {
+      console.log("valid", fields);
+    }
   };
 </script>
 
@@ -11,14 +39,17 @@
   <div class="form-field">
     <label for="question">Poll Question</label>
     <input type="text" id="question" bind:value={fields.question} />
+    <div class="error">{errors.question}</div>
   </div>
   <div class="form-field">
     <label for="answer-a">Answer A:</label>
     <input type="text" id="answer-a" bind:value={fields.answerA} />
+    <div class="error">{errors.answerA}</div>
   </div>
   <div class="form-field">
     <label for="answer-b">Answer B:</label>
     <input type="text" id="answer-b" bind:value={fields.answerB} />
+    <div class="error">{errors.answerB}</div>
   </div>
   <Button type="secondary" flat={true}>Add Poll</Button>
 </form>
@@ -40,5 +71,11 @@
   label {
     margin: 10px auto;
     text-align: left;
+  }
+
+  .error {
+    font-weight: bold;
+    color: #f93c6b;
+    font-size: 10px;
   }
 </style>
