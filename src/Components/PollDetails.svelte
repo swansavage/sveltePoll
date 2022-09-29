1c13e43a4d74629644e96520379ca5b729dfd326
swansavage@github.com <swansavage@gmail.com>
2022-09-29 18:05:25 -0500

    added functionality for reactive percentage of votes bars on Poll cards

--- src/Components/PollDetails.svelte
+++ src/Components/PollDetails.svelte
@@ -7,6 +7,8 @@
 
   // reactive values
   $: totalVotes = poll.votesA + poll.votesB;
+  $: percentA = Math.floor((100 / totalVotes) * poll.votesA);
+  $: percentB = Math.floor((100 / totalVotes) * poll.votesB);
 
   // handling votes
   const handleVote = (option, id) => {
@@ -19,11 +21,11 @@
     <h3>{poll.question}</h3>
     <p>Total votes: {totalVotes}</p>
     <div class="answer" on:click={() => handleVote("a", poll.id)}>
-      <div class="percent percent-a" />
+      <div class="percent percent-a" style="width: {percentA}%" />
       <span>{poll.answerA} ({poll.votesA})</span>
     </div>
     <div class="answer" on:click={() => handleVote("b", poll.id)}>
-      <div class="percent percent-b" />
+      <div class="percent percent-b" style="width: {percentB}%" />
       <span>{poll.answerB} ({poll.votesB})</span>
     </div>
   </div>
@@ -46,15 +48,33 @@
     background: #fafafa;
     cursor: pointer;
     margin: 10px auto;
+
     position: relative;
+    font-size: 14px;
   }
 
   .answer:hover {
     opacity: 0.6;
   }
 
-  .span {
+  span {
     display: inline-block;
     padding: 10px 20px;
+  }
+
+  .percent {
+    height: 100%;
+    position: absolute;
+    box-sizing: border-box;
+  }
+
+  .percent-a {
+    border-left: 4px solid #5ad2f9;
+    background: rgba(90, 210, 249, 0.3);
+  }
+
+  .percent-b {
+    border-left: 4px solid #f59c35;
+    background: rgba(245, 156, 53, 0.3);
   }
 </style>
