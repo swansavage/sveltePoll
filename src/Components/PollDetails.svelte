0eef2c0bb03931ce48be3a847ef371fa8b281e20
swansavage@github.com <swansavage@gmail.com>
2022-10-13 15:21:47 -0500

    added tweens for poll percentages to smoothly animate when vote is cast

--- src/Components/PollDetails.svelte
+++ src/Components/PollDetails.svelte
@@ -2,12 +2,21 @@
   import Card from "../Shared/Card.svelte";
   import PollStore from "../stores/PollStore";
   import Button from "../shared/Button.svelte";
+  import { tweened } from "svelte/motion";
   export let poll;
 
   // reactive values
   $: totalVotes = poll.votesA + poll.votesB;
-  $: percentA = Math.floor((100 / totalVotes) * poll.votesA);
-  $: percentB = Math.floor((100 / totalVotes) * poll.votesB);
+  $: percentA = Math.floor((100 / totalVotes) * poll.votesA) || 0;
+  $: percentB = Math.floor((100 / totalVotes) * poll.votesB) || 0;
+
+  // tweened percentages
+  const tweenedA = tweened(0);
+  const tweenedB = tweened(0);
+
+  $: tweenedA.set(percentA);
+  $: tweenedB.set(percentB);
+  // $: console.log($tweenedA, $tweenedB);
 
   // handling votes
   const handleVote = (option, id) => {
@@ -41,11 +50,11 @@
     <h3>{poll.question}</h3>
     <p>Total votes: {totalVotes}</p>
     <div class="answer" on:click={() => handleVote("a", poll.id)}>
-      <div class="percent percent-a" style="width: {percentA}%" />
+      <div class="percent percent-a" style="width: {$tweenedA}%" />
       <span>{poll.answerA} ({poll.votesA})</span>
     </div>
     <div class="answer" on:click={() => handleVote("b", poll.id)}>
-      <div class="percent percent-b" style="width: {percentB}%" />
+      <div class="percent percent-b" style="width: {$tweenedB}%" />
       <span>{poll.answerB} ({poll.votesB})</span>
     </div>
     <div class="delete">
